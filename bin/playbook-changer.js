9fc429e9b86a2fe0ffa1553764d8c874620aac4e
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-07-10 15:05:56 +0200

    Add script for changing the playbook references according to the environment

--- bin/playbook-changer.js
+++ bin/playbook-changer.js
@@ -1,13 +1,44 @@
 #!/bin/env node
+// Antora playbook changer script for Decidim documentation
+//
+// Changes the playbook accordingly to its environment
+//
+
+const fs = require('fs')
 
 const scriptName = process.argv[1];
-const environment = process.argv[2];
+const antoraPlaybook = "antora-playbook.yml";
 
-function helpMessage () {
-	console.log(`${scriptName} - Antora playbook changer script for Decidim documentation
+let headRef;
+let baseRef;
+let url;
 
-Changes the playbook accordingly to its environment`);
-	console.log(process.env);
+console.log(process.env);
+
+if (process.env.CI === "true") {
+  headRef = process.env.GITHUB_HEAD_REF;
+  baseRef = process.env.GITHUB_BASE_REF;
+} else {
+  headRef = "HEAD";
+  baseRef = "develop"
+  url = ".";
 }
 
-helpMessage();
+fs.readFile(antoraPlaybook, "utf8", function (err,data) {
+  if (err) {
+    return console.err(err);
+  }
+
+  let result = data.replace(baseRef, headRef);
+  console.log(url);
+  if (url != undefined) {
+    result = result.replace("https://github.com/decidim/documentation", url);
+  }
+
+  fs.writeFile(antoraPlaybook, result, "utf8", function (err) {
+    if (err) {
+      return console.err(err);
+    }
+  });
+});
+
